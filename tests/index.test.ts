7decdda479387711126deed4cd07ebe03b4da9af
Irshad Nilam <mail@irshadnilam.com>
2022-06-23 13:28:30 +0800

    Add filtering tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,14 +1,14 @@
-import * as fs from 'fs-extra';
-import { join } from 'path';
-import { Keypair } from '@solana/web3.js';
-import { Connection } from '@solana/web3.js';
-import { LAMPORTS_PER_SOL, sendAndConfirmTransaction, sendAndConfirmRawTransaction} from '@solana/web3.js';
-import { Constants, MSP, TreasuryType } from '../src';
-
-import { NATIVE_MINT } from '@solana/spl-token';
+import {Keypair} from '@solana/web3.js';
+import {Connection} from '@solana/web3.js';
+import {LAMPORTS_PER_SOL, sendAndConfirmRawTransaction, sendAndConfirmTransaction} from '@solana/web3.js';
 import {PublicKey} from "@solana/web3.js";
 import {Transaction} from "@solana/web3.js";
+import {Constants, MSP, TreasuryType} from '../src';
+import {Category} from "../src";
+
+import {NATIVE_MINT} from '@solana/spl-token';
 import {TimeUnit} from "../src/types";
+import {expect} from "chai";
 
 const endpoint = 'http://localhost:8899';
 // deploy msp locally
@@ -21,25 +21,27 @@ describe('Tests creating a vesting treasury\n', async () => {
   let user1Wallet: Keypair, user2Wallet: Keypair;
 
   before(async () => {
-    user1Wallet = await getKeyPair('test-user-1.json');
-    user2Wallet = await getKeyPair('test-user-2.json');
-
+    user1Wallet = Keypair.generate();
+    user2Wallet = Keypair.generate();
     connection = new Connection(endpoint, 'confirmed');
-
     await connection.confirmTransaction(
       await connection.requestAirdrop(
         user1Wallet.publicKey,
-        10000 * LAMPORTS_PER_SOL,
+        1000 * LAMPORTS_PER_SOL,
       ),
       'confirmed',
     );
+    console.log("Balance user1: : ", await connection.getBalance(user1Wallet.publicKey));
+
     await connection.confirmTransaction(
       await connection.requestAirdrop(
         user2Wallet.publicKey,
-        100000 * LAMPORTS_PER_SOL,
+        1000 * LAMPORTS_PER_SOL,
       ),
       'confirmed',
     );
+    console.log("Balance user2: : ", await connection.getBalance(user2Wallet.publicKey));
+
     msp = new MSP(endpoint, user1Wallet.publicKey.toBase58(), 'confirmed',
         new PublicKey("2nZ8KDGdPBexJwWznPZosioWJzNBSM3doUXUYdo37ndN"));
   });
@@ -96,15 +98,25 @@ describe('Tests creating a vesting treasury\n', async () => {
     });
     await sendAndConfirmRawTransaction(connection, createStreamTxSerialized, { commitment: 'confirmed' });
     console.log(`Stream created: ${stream.toBase58()}\n`);
+
+    console.log("Creating a non-vesting treasury");
+    const createTreasuryTx = await msp.createTreasury(
+        user1Wallet.publicKey,
+        user1Wallet.publicKey,
+        Constants.SOL_MINT,
+        "",
+        TreasuryType.Open
+    );
+    await sendAndConfirmTransaction(connection, createTreasuryTx, [user1Wallet], { commitment: 'confirmed' });
+    console.log("Non vesting treasury created\n");
+
+    console.log("Filtering treasury by category");
+    const filtered = await msp.listTreasuries(user1Wallet.publicKey, true, false, Category.vesting);
+    expect(filtered.length).eq(1);
+    expect(filtered.at(0)!.id).eq(treasury.toBase58());
+    console.log("Filter success.");
   });
 });
-
-// util functions
-const getKeyPair = async (path: string): Promise<Keypair> => {
-  const id = await fs.readJSON(join(__dirname, path));
-  const bytes = Uint8Array.from(id);
-  return Keypair.fromSecretKey(bytes);
-};
 
 const _printSerializedTx = (tx: Transaction, requireAllSignatures = false, verifySignatures = false) => {
   console.log(tx.serialize({
